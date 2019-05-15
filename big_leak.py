b2f5b90a289b992e2bbd5f74e0d6ea5eaf58a818
Clément Tarrière <clement.tarriere@gitguardian.com>
2019-05-15 14:08:53 +0200

    Not a postgres 2

--- big_leak.py
+++ big_leak.py
@@ -1,2 +1,6 @@
 new_token = "hG6z2yC1-5AQviaGwmUA"
-db_host=w1rEKgG0DJ8yckhy7HzzAEnPmABGENKAWZ0iZ2CgbEcSeERl-C7pGirmOWKHP54ri6XTvZ-.com\ndb_port=9242\ndb_username=iev0IKGKLrT\ndb_password=vjp*wlqySwdWjtTr74tI0MUvwvbP9
+db_host=w1rEKgG0DJ8yckhy7HzzAEnPmABGENKAWZ0iZ2CgbEcSeERl-C7pGirmOWKHP54ri6XTvZ-.com
+db_port=9242
+db_username=iev0IKGKLrT
+db_password=vjp*wlqySwdWjtTr74tI0MUvwvbP9
+
